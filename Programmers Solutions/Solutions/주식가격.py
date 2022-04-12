7b7ba2950a6d635aef22d11e391a0233d9decfe0
MunSeungWoo <city1616@naver.com>
2022-04-12 21:28:52 +0900

    Add Solution

--- Programmers Solutions/Solutions/주식가격.py
+++ Programmers Solutions/Solutions/주식가격.py
@@ -1,30 +1,33 @@
 from collections import deque
 
 def solution(prices) :
-    q = deque()
-    e = deque()
-    result = []
-    # q.append(price for price in prices)
-    for price in prices :
-        q.append(price)
-    
-    print("q :", q)
+    answer = []
+    for i in range(len(prices) - 1) :
+        time = len(prices) - i - 1
+        for j in range(i + 1, len(prices)) :
+            if prices[i] > prices[j] :
+                time = j - i
+                break
+        answer.append(time)
+    answer.append(0)
+    print("answer :", answer)
+    return answer
 
-    for _ in range(len(q)) :
-        tmp = q.pop()
-        if not e :
-            result.append(0)
-        else :
-            cnt = 0
-            for i in range(len(e)) :
-                if tmp <= e[i] :
-                    cnt += 1
-            result.append(cnt)
-        e.append(tmp)
-    result = list(reversed(result))
-    print("q :", q)
-    print("e :", e)
-    print("result :", result)
-    return result
+solution([1, 2, 3, 2, 3])
+
+# Solution 2
+
+def solution(prices) :
+    answer = [0] * len(prices)
+    print(answer)
+    for i in range(len(prices)) :
+        for j in range(i + 1, len(prices)) :
+            if prices[i] <= prices[j] :
+                answer[i] += 1
+            else :
+                answer [i] += 1
+                break
+    print("answer :", answer)
+    return answer
 
 solution([1, 2, 3, 2, 3])

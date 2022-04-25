bf1aafbe09dcc9e4e63d66318a9414205ac92b81
MunSeungWoo <city1616@naver.com>
2022-04-25 22:18:22 +0900

    Add Solution

--- Programmers Solutions/Solutions/다리를 지나는 트럭.py
+++ Programmers Solutions/Solutions/다리를 지나는 트럭.py
@@ -14,4 +14,4 @@ def solution(bridge_length, weight, truck_weights) :
 
 solution(2, 10, [7, 4, 5, 6])  
 # solution(100, 100, [10]) 
-# solution(100, 100, [10,10,10,10,10,10,10,10,10,10])                         
+# solution(100, 100, [10,10,10,10,10,10,10,10,10,10])

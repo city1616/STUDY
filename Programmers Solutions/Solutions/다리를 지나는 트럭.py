8d650948bb52bcf7e129eb31cdb056a7355b0ffe
MunSeungWoo <city1616@naver.com>
2022-04-25 23:52:11 +0900

    Add Solution

--- Programmers Solutions/Solutions/다리를 지나는 트럭.py
+++ Programmers Solutions/Solutions/다리를 지나는 트럭.py
@@ -1,17 +1,35 @@
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights) :
-    answer = 0
+    i = 0
+    cnt = 1
     moving = deque()
-    end = deque()
-    for truck_weight in truck_weights :
-        if sum(moving) >= weight :
-            moving.append(truck_weight)
-            answer += 1
+    z = deque([0 for _ in range(bridge_length)])
+    sum_weight = 0
+    
+    while True :
+        if i == len(truck_weights) :
+            moving.appendleft(0)
+        elif weight - sum_weight >= truck_weights[i] :
+            moving.appendleft(truck_weights[i])
+            sum_weight += truck_weights[i]
+            i += 1
+        else :
+            moving.appendleft(0)
 
-    print("moving :", moving)
-    return answer
+        # print("cnt :", cnt, "moving :", moving, "z :", z)
+        if moving == z :
+            break
+        
+        if len(moving) == bridge_length :
+            sum_weight -= moving.pop()
+
+        cnt += 1
+    
+    print("answer :", cnt)
+    
+    return cnt
 
 solution(2, 10, [7, 4, 5, 6])  
-# solution(100, 100, [10]) 
-# solution(100, 100, [10,10,10,10,10,10,10,10,10,10])
+solution(100, 100, [10]) 
+solution(100, 100, [10,10,10,10,10,10,10,10,10,10])

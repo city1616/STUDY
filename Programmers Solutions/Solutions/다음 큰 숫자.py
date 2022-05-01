207d743b7633c2890fa74b41d99fa50c3f18dd6b
MunSeungWoo <city1616@naver.com>
2022-05-01 23:05:57 +0900

    Add Solution

--- Programmers Solutions/Solutions/다음 큰 숫자.py
+++ Programmers Solutions/Solutions/다음 큰 숫자.py
@@ -1,5 +1,12 @@
 def solution(n) :
-    answer = 0
+    one_cnt = list(map(int, bin(n)[2 : ])).count(1)
+    while True :
+        n += 1
+        if list(map(int, bin(n)[2 : ])).count(1) == one_cnt :
+            print("n :", n)
+            answer = n
+            break
+    
     return answer
 
 

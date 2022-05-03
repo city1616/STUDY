016b285426be1d1fe3c8756bade162e8256f2d5c
MunSeungWoo <city1616@naver.com>
2022-05-03 20:24:25 +0900

    Add Solution

--- Programmers Solutions/Solutions/더 맵게.py
+++ Programmers Solutions/Solutions/더 맵게.py
@@ -3,13 +3,21 @@ import heapq
 def solution(scoville, K) :
     hq = []
     cnt = 0
+    list_length = len(scoville)
     for i in scoville :
         heapq.heappush(hq, i)
     while hq[0] < K :
         heapq.heappush(hq, heapq.heappop(hq) + (heapq.heappop(hq) * 2))
         cnt += 1
+        if len(hq) == 1 :
+            break
     print("count :", cnt)
     print("hq :", hq)
-    return cnt
+    if hq[0] < K :
+        return -1
+    else :
+        return cnt
+    
 
 solution([1, 2, 3, 9, 10, 12], 7) # 2
+solution([1, 1, 1], 3)

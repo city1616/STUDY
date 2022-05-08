53139d53fcfa54296789808f8bbf93f88ead9e30
MunSeungWoo <city1616@naver.com>
2022-05-08 12:51:52 +0900

    Add Solution

--- Programmers Solutions/Solutions/피보나치 수.py
+++ Programmers Solutions/Solutions/피보나치 수.py
@@ -1,6 +1,15 @@
 def solution(n) :
-    answer = 0
+    answer = Fibo(n) % 1234567
+    print(Fibo(n))
     return answer
+
+def Fibo(a) :
+    if a == 0 :
+        return 0
+    elif a == 1 :
+        return 1
+    else :
+        return Fibo(a - 1) + Fibo(a - 2)
 
 solution(3) # 2
 solution(5) # 5

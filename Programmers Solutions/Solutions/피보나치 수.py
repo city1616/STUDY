6ad935ac11e55feec7a707e56ad5364973baad73
MunSeungWoo <city1616@naver.com>
2022-05-10 18:09:16 +0900

    Add Solution

--- Programmers Solutions/Solutions/피보나치 수.py
+++ Programmers Solutions/Solutions/피보나치 수.py
@@ -1,17 +1,37 @@
-def solution(n) :
-    answer = Fibo(n) % 1234567
-    print(Fibo(n))
-    return answer
+# solution 1
 
-def Fibo(a) :
-    if a == 0 :
-        return 0
-    elif a == 1 :
-        return 1
-    else :
-        return (Fibo(a - 1) % 1234567) + (Fibo(a - 2) % 1234567)
+def solution(n) :
+    fibo = [0, 1]
+    for i in range(2, n + 1) :
+        fibo.append(fibo[i - 2] + fibo[i - 1])
+
+    print(fibo[n])
+    return fibo[n] % 1234567
+
+# 시간초과
+# def solution(n) :
+#     answer = Fibo(n) % 1234567
+#     print(Fibo(n))
+#     return answer
+
+# def Fibo(a) :
+#     if a == 0 :
+#         return 0
+#     elif a == 1 :
+#         return 1
+#     else :
+#         return (Fibo(a - 1) % 1234567) + (Fibo(a - 2) % 1234567)
 
 solution(3) # 2
 solution(5) # 5
 solution(100000)
  
+# solution 2
+
+def fibonacci(n) :
+    a, b = 0, 1
+    for i in range(n) :
+        a, b = b, a + b
+    return a
+
+print(fibonacci(3))

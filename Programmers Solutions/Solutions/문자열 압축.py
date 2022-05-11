28f4d4cd07ed13ccbcfe7fcbeec0909f8497fb65
MunSeungWoo <city1616@naver.com>
2022-05-11 22:33:15 +0900

    Add Solution

---
+++ Programmers Solutions/Solutions/문자열 압축.py
@@ -0,0 +1,21 @@
+def solution(s) :
+    answer = 0
+
+    print("s :", s)
+    s_dict = {}
+    for c in s :
+        if c not in s_dict :
+            s_dict[c] = 1
+        else :
+            s_dict[c] += 1
+    
+    print("s_dict :", s_dict)
+
+    return answer
+
+
+solution("aabbaccc") # 7
+# solution("ababcdcdababcdcd") # 9
+# solution("abcabcdede") # 8
+# solution("abcabcabcabcdededededede") # 14
+# solution("xababcdcdababcdcd") # 17

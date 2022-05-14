f0ac86fe09b9cee42f7ba2667cbd0ebb2062da79
MunSeungWoo <city1616@naver.com>
2022-05-14 13:03:00 +0900

    Add Solution

--- Programmers Solutions/Solutions/문자열 압축.py
+++ Programmers Solutions/Solutions/문자열 압축.py
@@ -13,7 +13,7 @@ def solution(s) :
     
     print("s_dict :", s_dict)
 
-    print("length :", len(s))
+    print("length :", len(s)) 
 
     for i in range(1, len(s) + 1) :
         print("i :", i)

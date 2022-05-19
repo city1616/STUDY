39a470aafa44e099191771948b70deca31fc2fe4
MunSeungWoo <city1616@naver.com>
2022-05-19 23:50:50 +0900

    Add Solution

--- Programmers Solutions/Solutions/전화번호 목록.py
+++ Programmers Solutions/Solutions/전화번호 목록.py
@@ -1,15 +1,16 @@
 def solution(phone_book) :
-    phone_book = sorted(phone_book, key = lambda x : len(x))
+
+    phone_book = sorted(phone_book, key = lambda x : len(x)) # 길이 순서대로 정렬
     print("phone_book :", phone_book)
 
     for i in range(len(phone_book)) :
         for j in range(i + 1, len(phone_book)) :
             if len(phone_book[i]) == len(phone_book[j]) :
                 continue
-            if phone_book[i] == phone_book[j][:len(phone_book[i])] :
+            elif phone_book[i] == phone_book[j][:len(phone_book[i])] :
                 return False
     return True
 
 solution(["119", "97674223", "1195524421"]) # false
-solution(["123","456","789"]) # true
-solution(["12","123","1235","567","88"]) # false
+# solution(["123","456","789"]) # true
+# solution(["12","123","1235","567","88"]) # false

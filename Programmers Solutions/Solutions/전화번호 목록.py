183aaf43a2b758f85a34fa148d121941c723d1df
MunSeungWoo <city1616@naver.com>
2022-05-20 23:57:13 +0900

    Add Solution

--- Programmers Solutions/Solutions/전화번호 목록.py
+++ Programmers Solutions/Solutions/전화번호 목록.py
@@ -5,12 +5,14 @@ def solution(phone_book) :
 
     for i in range(len(phone_book)) :
         for j in range(i + 1, len(phone_book)) :
+            
             if len(phone_book[i]) == len(phone_book[j]) :
                 continue
             elif phone_book[i] == phone_book[j][:len(phone_book[i])] :
                 return False
+
     return True
 
 solution(["119", "97674223", "1195524421"]) # false
 # solution(["123","456","789"]) # true
-# solution(["12","123","1235","567","88"]) # false
+# solution(["12","123","1235","567","88"]) # false 

227e9db1a354be0177ed7f6f7f297ff56fdc8e48
MunSeungWoo <city1616@naver.com>
2022-05-21 11:39:22 +0900

    Add Solution

--- Programmers Solutions/Solutions/전화번호 목록.py
+++ Programmers Solutions/Solutions/전화번호 목록.py
@@ -14,3 +14,34 @@ def solution(phone_book) :
 solution(["119", "97674223", "1195524421"]) # false
 # solution(["123","456","789"]) # true
 # solution(["12","123","1235","567","88"]) # false 
+
+# def solution(phone_book):
+#     # 정렬
+#     phone_book.sort(key=len)
+#     hash_table = {}
+
+#     # 맨 처음 가장 짧은 길이를 저장. min.
+#     min_len = len(phone_book[0])
+
+#     for target in phone_book:
+#         # loop마다 본인 정보를 hash table에 저장.
+#         hash_table[hash(target)] = target
+#         # min길이부터 +1씩 해서 본인 길이까지 hash table에서 검색.
+#         # 있으면 false, 없으면 true-계속 진행.
+#         for i in range(min_len,len(target)):
+#             try:
+#                 if hash_table[hash(target[0:i])]:
+#                     return False
+#             except KeyError:
+#                 print('key error')
+#                 # 아무 동작 안 해도 됨.
+
+#     return True
+
+# def solution(phoneBook):
+#     phoneBook = sorted(phoneBook)
+
+#     for p1, p2 in zip(phoneBook, phoneBook[1:]):
+#         if p2.startswith(p1):
+#             return False
+#     return True

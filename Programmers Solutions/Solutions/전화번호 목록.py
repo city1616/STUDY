95453478f434625c3115208547f930eeb47797cf
MunSeungWoo <city1616@naver.com>
2022-05-21 11:26:18 +0900

    Add Solution

--- Programmers Solutions/Solutions/전화번호 목록.py
+++ Programmers Solutions/Solutions/전화번호 목록.py
@@ -5,10 +5,10 @@ def solution(phone_book) :
 
     for i in range(len(phone_book)) :
         for j in range(i + 1, len(phone_book)) :
-            
+
             if len(phone_book[i]) == len(phone_book[j]) :
                 continue
-            elif phone_book[i] == phone_book[j][:len(phone_book[i])] :
+            elif phone_book[i] == phone_book[j][: len(phone_book[i])] :
                 return False
 
     return True

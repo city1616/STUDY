8ee5391d5c64d2a66346246d537b20268739bb1a
MunSeungWoo <city1616@naver.com>
2022-06-14 23:43:44 +0900

    Add Solution

--- SW Expert Academy/Solutions/어디에 단어가 들어갈 수 있을까.py
+++ SW Expert Academy/Solutions/어디에 단어가 들어갈 수 있을까.py
@@ -2,22 +2,7 @@ T = int(input())
 
 for test_case in range(1, T + 1) :
     N, K = map(int, input().split())
-    p = []
-    for _ in range(N) :
-        tmp = list(map(int, input().split()))
-        p.append(tmp)
     
-    cnt = 0
-    result = 0
-
-    for i in range(N) :
-        for j in range(N) :
-            if p[i][j] == 1 :
-                cnt += 1
-            else :
-                cnt = 0
-
-    print("cnt :", cnt)
     print("#" + str(test_case)) 
 
 #1 2

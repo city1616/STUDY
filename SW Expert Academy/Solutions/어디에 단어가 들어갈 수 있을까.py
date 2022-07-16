c29ca5658e6f669c4b1fee96eada073c39f7faee
MunSeungWoo <city1616@naver.com>
2022-07-17 00:17:14 +0900

    Add Solution

--- SW Expert Academy/Solutions/어디에 단어가 들어갈 수 있을까.py
+++ SW Expert Academy/Solutions/어디에 단어가 들어갈 수 있을까.py
@@ -1,9 +1,35 @@
+import sys
+
+sys.stdin = open("../Input/input_1979.txt", "r")
+
 T = int(input())
 
 for test_case in range(1, T + 1) :
     N, K = map(int, input().split())
+    arr = []
+    row = ""
+    col = ""
+    total = []
+    cnt = 0
+
+    for i in range(N) :
+        arr.append(list(map(int, input().split())))
+
+    for i in range(N) :
+        for j in range(N) :
+            row += str(arr[i][j])
+            col += str(arr[j][i])
+        total.append(row)
+        total.append(col)
+        row = ""
+        col = ""  
+    
+    for i in range(len(total)) :
+        for j in total[i].split("0") :
+            if len(j) == K :
+                cnt += 1
      
-    print("#" + str(test_case))
+    print("#" + str(test_case), cnt)
     
 
 #1 2

83d4e52aeb0084ec89f5a3baa1968d7830fc9a51
city1616 <city1616@naver.com>
2022-07-23 00:30:02 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java_Algo/src/algo/_10172_개.java
@@ -0,0 +1,13 @@
+package algo;
+
+public class _10172_개 {
+
+	public static void main(String[] args) {
+		System.out.println("|\\_/|\n" + 
+				           "|q p|   /}\n" + 
+				           "( 0 )\"\"\"\\\n" + 
+				           "|\"^\"`    |\n" + 
+			        	   "||_/=\\\\__|");
+	}
+
+}

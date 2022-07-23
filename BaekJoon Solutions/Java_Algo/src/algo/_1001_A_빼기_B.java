0f4a32fb8bf225c14b439dac7610a171fedc2e00
city1616 <city1616@naver.com>
2022-07-23 23:39:43 +0900

    Add Solution

--- BaekJoon Solutions/Java_Algo/src/algo/_1001_A_빼기_B.java
+++ BaekJoon Solutions/Java_Algo/src/algo/_1001_A_빼기_B.java
@@ -3,7 +3,6 @@ package algo;
 import java.util.Scanner;
 
 public class _1001_A_빼기_B {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
@@ -11,5 +10,4 @@ public class _1001_A_빼기_B {
 		System.out.println(a - b);
 		sc.close();
 	}
-
 }

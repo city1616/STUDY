d695eaa22a4b6bc5ead0183727b6dbdd4cd5464c
city1616 <city1616@naver.com>
2022-07-24 18:50:46 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java_Algo/src/algo/_10926_물음표물음표느낌표.java
@@ -0,0 +1,12 @@
+package algo;
+
+import java.util.Scanner;
+
+public class _10926_물음표물음표느낌표 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String id = sc.next();
+		System.out.println(id + "??!");
+		sc.close();
+	}
+}
---
+++ BaekJoon Solutions/Java_Algo/src/algo/_1330_두_수_비교하기.java
@@ -0,0 +1,21 @@
+package algo;
+
+import java.util.Scanner;
+
+public class _1330_두_수_비교하기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		String result = null;
+		if(A > B) {
+			result = ">";
+		} else if(A < B) {
+			result = "<";
+		} else {
+			result = "==";
+		}
+		System.out.println(result);
+		sc.close();
+	}
+}

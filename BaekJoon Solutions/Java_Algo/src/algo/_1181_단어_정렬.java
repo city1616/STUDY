60086e4d47059514335606cd3f76a7c35b3a10b5
city1616 <city1616@naver.com>
2022-07-28 15:31:24 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java_Algo/src/algo/_1181_단어_정렬.java
@@ -0,0 +1,16 @@
+package algo;
+
+import java.util.Scanner;
+
+public class _1181_단어_정렬 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		String[] word = new String[N];
+		for(int i = 0; i < N; i++) {
+			word[i] = sc.next();
+		}
+		
+	}
+}
---
+++ BaekJoon Solutions/Java_Algo/src/algo/_15552_빠른_A_더하기_B.java
@@ -0,0 +1,23 @@
+package algo;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class _15552_빠른_A_더하기_B {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			// int A = Integer.parseInt(br.readLine());
+			// int B = Integer.parseInt(br.readLine());
+			
+			bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
+			// bw.flush();
+		}
+		bw.close();
+		//System.out.println(sum);
+	}
+}

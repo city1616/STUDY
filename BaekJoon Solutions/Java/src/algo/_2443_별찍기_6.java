b92f71fe8288ebc4e5f09f3f4657d47b70c6acd1
city1616 <city1616@naver.com>
2022-07-30 00:05:05 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_2443_별찍기_6.java
@@ -0,0 +1,20 @@
+package algo;
+
+import java.util.Scanner;
+
+public class _2443_별찍기_6 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j < (N - i - 1) * 2 + 1; j++) { // for(int j = i; j < (9 - i); j++)
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}

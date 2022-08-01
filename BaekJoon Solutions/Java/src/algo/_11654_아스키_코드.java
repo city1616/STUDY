2b440e595ee1f3b5bfd7ea96410d8e853536246a
city1616 <city1616@naver.com>
2022-08-01 10:13:41 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_11654_아스키_코드.java
@@ -0,0 +1,11 @@
+package algo;
+
+import java.io.*;
+
+public class _11654_아스키_코드 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char c = br.readLine().charAt(0);
+		System.out.println(Integer.valueOf(c));
+	}
+}

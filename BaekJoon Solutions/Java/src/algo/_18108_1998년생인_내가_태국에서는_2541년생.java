14914d7cd63c7b5fb82bebeee0298c4213368e46
city1616 <city1616@naver.com>
2022-08-01 10:13:51 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_18108_1998년생인_내가_태국에서는_2541년생.java
@@ -0,0 +1,14 @@
+package algo;
+
+import java.util.Scanner;
+
+public class _18108_1998년생인_내가_태국에서는_2541년생 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int year = sc.nextInt();
+		System.out.println(year - 543);
+		
+		sc.close();
+	}
+}

49d52d8bc01cbd883ac9fb256e55b4fd0cd2d4bc
city1616 <city1616@naver.com>
2022-08-10 23:59:12 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_1075_나누기.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class _1075_나누기 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int F = Integer.parseInt(br.readLine());
+		N -= (N % 100);
+		while(true) {
+			if(N % F == 0) {
+				break;
+			}
+			N++;
+		}
+		System.out.printf("%02d", (N % 100));
+		br.close();
+	}
+}

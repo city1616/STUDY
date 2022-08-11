001cea4959fab19e89b454357ee98237b774e88f
city1616 <city1616@naver.com>
2022-08-11 19:41:39 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_1158_요세푸스_문제.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class _1158_요세푸스_문제 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		ArrayDeque<Integer> q = new ArrayDeque<>();
+		int[] ans = new int[N];
+		
+		for(int i = 1; i <= N; i++) {
+			q.offer(i);
+		}
+		
+		int i = 0;
+		sb.append("<");
+		while(q.size() > 1) {
+			for(int x = 0; x < (K - 1); x++) {
+				q.offer(q.poll());
+			}
+			sb.append(q.poll()).append(", ");
+		}
+		sb.append(q.poll()).append(">");
+		System.out.println(sb);
+		br.close();
+	}
+}
+
+/*
+
+1 2 3 4 5 6 7
+3 6 2 7 5 1 4
+
+*/

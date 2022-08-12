0fd64c9ec2e48c75648871ad249a90a8a3da4a49
city1616 <city1616@naver.com>
2022-08-13 00:54:29 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_1094_막대기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class _1094_막대기 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int X = Integer.parseInt(br.readLine());
+		int[] arr = {64, 32, 16, 8, 4, 2, 1};
+		int cnt = 0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			if(X >= arr[i]) {
+				X -= arr[i];
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+		br.close();
+	}
+}

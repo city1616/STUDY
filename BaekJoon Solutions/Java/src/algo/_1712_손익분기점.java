d71d06662f93d56d5d43e625315552ce8144ecb8
city1616 <city1616@naver.com>
2022-08-27 00:09:21 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_1712_손익분기점.java
@@ -0,0 +1,22 @@
+package algo;
+
+import java.io.*;
+import java.util.*;
+
+public class _1712_손익분기점 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		long A = Integer.parseInt(st.nextToken());
+		long B = Integer.parseInt(st.nextToken());
+		long C = Integer.parseInt(st.nextToken());
+		long cnt = -1;
+		if(B >= C) {
+			cnt = -1;
+		} else {
+			cnt = A / (C - B) + 1;
+		}
+		System.out.println(cnt);
+		br.close();
+	}
+}

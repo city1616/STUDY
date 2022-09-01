9444c2b9b222414565fa1538b1d330c105f9f09f
city1616 <city1616@naver.com>
2022-09-01 22:33:53 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_1037_약수.java
@@ -0,0 +1,25 @@
+package algo;
+
+import java.io.*;
+import java.util.*;
+
+public class _1037_약수 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		int result;
+		if(arr.length == 1) {
+			result = arr[0] * arr[0];
+		} else {
+			Arrays.sort(arr);
+			result = arr[0] * arr[N - 1];
+		}
+		System.out.println(result);
+		br.close();
+	}
+}

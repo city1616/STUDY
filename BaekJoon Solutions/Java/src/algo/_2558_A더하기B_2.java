3e092bc5a3dfedcb4cc55454a0b1088a25090c67
city1616 <city1616@naver.com>
2022-09-24 00:35:53 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_2558_A더하기B_2.java
@@ -0,0 +1,15 @@
+package algo;
+
+import java.io.*;
+import java.util.*;
+
+public class _2558_A더하기B_2 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		System.out.println(N + M);
+		br.close();
+	}
+
+}

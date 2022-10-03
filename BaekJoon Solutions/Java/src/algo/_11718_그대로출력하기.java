1c757d5eec6c06262a7d7a608ffe3178badfaf86
city1616 <city1616@naver.com>
2022-10-03 11:09:18 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_11718_그대로출력하기.java
@@ -0,0 +1,18 @@
+package algo;
+
+import java.io.*;
+import java.util.*;
+
+public class _11718_그대로출력하기 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		String line;
+		while((line = br.readLine()) != null) {
+			sb.append(line).append("\n");
+		}
+		System.out.print(sb);
+		br.close();
+	}
+}

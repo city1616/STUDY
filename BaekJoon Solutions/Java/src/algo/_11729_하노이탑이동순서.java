ee373d51ee38f8301745e11ec6b3ea1c0e3004eb
city1616 <city1616@naver.com>
2023-01-03 23:08:23 +0900

    Add Solution

--- BaekJoon Solutions/Java/src/algo/_11729_하노이탑이동순서.java
+++ BaekJoon Solutions/Java/src/algo/_11729_하노이탑이동순서.java
@@ -3,19 +3,33 @@ package algo;
 import java.io.*;
 
 public class _11729_하노이탑이동순서 {
+	
+	static StringBuilder sb = new StringBuilder();
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		int N = Integer.parseInt(br.readLine());
+		sb.append((int) (Math.pow(2, N) - 1)).append("\n");
+		hanoi(N, 1, 2, 3);
 		
-		
-		
-		br.close();
+		System.out.println(sb);
 	}
 	
-	static void hanoi(int n) {
-		if(n == 1) {
+	static void hanoi(int N, int start, int mid, int to) {
+		if(N == 1) {
+			sb.append(start + " " + to + "\n");
 			return;
 		}
-		hanoi(n - 1);
+		// A -> C로 옮긴다고 가정
+		
+		// N - 1개를 A에서 B로 이동
+		hanoi(N - 1, start, to, mid);
+		
+		// 1개를 A에서 C로 이동
+		sb.append(start + " " + to + "\n");
+		
+		// N - 1개를 B에서 C로 이동
+		hanoi(N - 1, mid, start, to);
 	}
 }

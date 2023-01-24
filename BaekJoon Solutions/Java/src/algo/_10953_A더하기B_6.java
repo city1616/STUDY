4dd13f1c657abc75c74b11ce828fb2f14639c4ef
city1616 <city1616@naver.com>
2023-01-24 20:03:18 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_10953_A더하기B_6.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class _10953_A더하기B_6 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++) {
+            String S = br.readLine();
+            sb.append(S.charAt(0) + S.charAt(2) - 96).append("\n");
+        }
+        System.out.print(sb);
+    }
+}

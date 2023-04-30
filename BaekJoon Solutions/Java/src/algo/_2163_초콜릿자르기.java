a2a207843c88dcb4ae02ef8337c7ba7c7d83bd0b
city1616 <city1616@naver.com>
2023-04-30 22:20:27 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_2163_초콜릿자르기.java
@@ -0,0 +1,16 @@
+package algo;
+
+import java.io.*;
+import java.util.*;
+
+public class _2163_초콜릿자르기 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        System.out.println(N * M - 1);
+        br.close();
+    }
+}

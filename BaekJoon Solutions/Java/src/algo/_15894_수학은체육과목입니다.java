38d745e8d55e9ddcee9d7ded0cc2339a22e27983
city1616 <city1616@naver.com>
2023-05-04 23:52:50 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_15894_수학은체육과목입니다.java
@@ -0,0 +1,12 @@
+package algo;
+
+import java.io.*;
+
+public class _15894_수학은체육과목입니다 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Integer.parseInt(br.readLine());
+        System.out.println(N * 4);
+        br.close();
+    }
+}

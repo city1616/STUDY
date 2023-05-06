6d0ce01da17617df5d1399d816c5950b1c97eef3
city1616 <city1616@naver.com>
2023-05-07 00:22:06 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_14215_세막대.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class _14215_세막대 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[3];
+        for(int i = 0; i < 3; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        while(arr[2] >= arr[1] + arr[0]) arr[2] -= 1;
+
+        System.out.println(arr[0] + arr[1] + arr[2]);
+        br.close();
+    }
+}

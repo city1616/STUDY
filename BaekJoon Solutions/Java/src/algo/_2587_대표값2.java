146c036d478ef207aa7817e4540d4fd64d97703d
city1616 <city1616@naver.com>
2023-05-08 21:46:53 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_2587_대표값2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class _2587_대표값2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[5];
+        int sum = 0;
+
+        for(int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+        System.out.println(sum / 5);
+
+        Arrays.sort(arr);
+        System.out.println(arr[2]);
+    }
+}

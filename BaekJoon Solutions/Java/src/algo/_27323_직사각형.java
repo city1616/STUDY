3966dd7dc5f46c68fac87a7d20bad8d9fe72623d
city1616 <city1616@naver.com>
2023-05-10 22:20:54 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_27323_직사각형.java
@@ -0,0 +1,13 @@
+package algo;
+
+import java.io.*;
+
+public class _27323_직사각형 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        System.out.println(A * B);
+        br.close();
+    }
+}

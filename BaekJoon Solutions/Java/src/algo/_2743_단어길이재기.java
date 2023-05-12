019f2bf3e4c94a94b00f0f2d5355b201f4e9a42c
city1616 <city1616@naver.com>
2023-05-12 23:52:48 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_2743_단어길이재기.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class _2743_단어길이재기 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        System.out.println(S.length());
+    }
+}

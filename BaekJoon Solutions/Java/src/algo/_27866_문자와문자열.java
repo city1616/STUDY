eb14b342cf4d893afd9a9bf0c34cc6003b9feb20
city1616 <city1616@naver.com>
2023-05-14 23:59:11 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_27866_문자와문자열.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class _27866_문자와문자열 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+        System.out.println(S.charAt(i - 1));
+    }
+}

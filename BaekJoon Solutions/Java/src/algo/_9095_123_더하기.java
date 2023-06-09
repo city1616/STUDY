b02acab7e2d2e48cd6892db9af6aa626a164857c
city1616@naver.com <city1616@naver.com>
2023-06-09 16:34:30 +0900

    Add Solutions

---
+++ BaekJoon Solutions/Java/src/algo/_9095_123_더하기.java
@@ -0,0 +1,37 @@
+package algo;
+
+import java.io.*;
+
+public class _9095_123_더하기 {
+
+    static int ans;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int tc = 0; tc < T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            ans = 0;
+            cal(0, N);
+
+            sb.append(ans).append("\n");
+        }
+        System.out.print(sb);
+        br.close();
+    }
+
+    static void cal(int cnt, int N) {
+        if(cnt > N) return;
+
+        if(cnt == N) {
+            ans += 1;
+            return;
+        }
+        cal(cnt + 1, N);
+        cal(cnt + 2, N);
+        cal(cnt + 3, N);
+    }
+}

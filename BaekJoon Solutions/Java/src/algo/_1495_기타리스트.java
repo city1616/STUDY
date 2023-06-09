1930fd6352832c2e4581818ca5d3e202da9bb6be
city1616@naver.com <city1616@naver.com>
2023-06-09 16:34:02 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_1495_기타리스트.java
+++ BaekJoon Solutions/Java/src/algo/_1495_기타리스트.java
@@ -20,17 +20,27 @@ public class _1495_기타리스트 {
         for(int i = 0;  i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
 
         ans = -1;
-        vol(0, S);
+
+        // 행 index : 연주해야될 인덱스, 열 index : 현재 볼륨
+        boolean[][] dp = new boolean[N][M + 1];
+
+        if(S - arr[0] >= 0) dp[0][S - arr[0]] = true;
+        if(S + arr[0] <= M) dp[0][S + arr[0]] = true;
+
+        for(int i = 0; i < N - 1; i++) {
+            for(int j = 0; j <= M; j++) {
+                if(!dp[i][j]) continue;
+                if(j - arr[i + 1] >= 0) dp[i + 1][j - arr[i + 1]] = true; // 점화식
+                if(j + arr[i + 1] <= M) dp[i + 1][j + arr[i + 1]] = true; // 점화식
+            }
+        }
+
+        // 마지막 연주가 끝난 후 마지막 행에서 값이 true인 가장 큰 열 인덱스를 구한다.
+        for(int i = 0; i <= M; i++) {
+            if(dp[N - 1][i] && i > ans) ans = i;
+        }
+
         System.out.println(ans);
         br.close();
-    }
-
-    static void vol(int i, int now) {
-        if(i >= N) {
-            ans = Math.max(ans, now);
-            return;
-        }
-        if(now + arr[i] <= M) vol(i + 1, now + arr[i]);
-        if(now - arr[i] >= 0) vol(i + 1, now - arr[i]);
     }
 }

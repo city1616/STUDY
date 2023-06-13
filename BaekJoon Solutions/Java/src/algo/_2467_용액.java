52dc8528784d857189d3573dba6598b10a7df5f4
city1616 <city1616@naver.com>
2023-06-13 21:21:36 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_2467_용액.java
@@ -0,0 +1,35 @@
+package algo;
+
+import java.io.*;
+import java.util.*;
+
+public class _2467_용액 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());    // 용액 개수
+        int[] water = new int[N];                   // 용액 값
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++) water[i] = Integer.parseInt(st.nextToken());
+
+        int start = 0;      // 왼쪽 인덱스 번호
+        int end = N - 1;    // 오른쪽 인덱스 번호
+
+        int minStart = 0;   // 용액의 차이가 최소일 때의 왼쪽 인덱스 번호
+        int minEnd = 0;     // 용액의 차이가 최소일 때의 오른쪽 인덱스 번호
+        int minDiff = Integer.MAX_VALUE; // 차이의 최솟값
+
+        while(start < end) {
+            if(Math.abs(water[start] + water[end]) < minDiff) {  // 용액의 차이가 최소가 된다면
+                minStart = start;        // 인덱스 번호 갱신
+                minEnd = end;            // 인덱스 번호 갱신
+                minDiff = Math.abs(water[start] + water[end]); // 최솟값 갱신
+            }
+
+            if(water[start] + water[end] > 0) end -= 1;     // 용액의 합이 0보다 크면 가리키는 값을 낮추기 위해 오른쪽 인덱스를 하나 낮춘다.
+            else start += 1;                                // 용액의 합이 0보다 작으면 가리키는 값을 높이기 위해 왼쪽 인덱스를 하나 올린다.
+        }
+
+        System.out.println(water[minStart] + " " + water[minEnd]);
+        br.close();
+    }
+}

2e8223bc13581b0479c2f3491baf722c1c2ddfa9
city1616 <city1616@naver.com>
2023-06-15 21:15:13 +0900

    Add Solution

--- BaekJoon Solutions/Java/src/algo/_1654_랜선자르기.java
+++ BaekJoon Solutions/Java/src/algo/_1654_랜선자르기.java
@@ -15,34 +15,32 @@ public class _1654_랜선자르기 {
 
         Arrays.sort(wire);
 
+        boolean flag = true;
+        for(int i = 0; i < K - 1; i++) {
+            if(wire[i] != wire[i + 1]) {
+                flag = false;
+                break;
+            }
+        }
+
         long start = 0;
         long end = wire[K - 1];
         long half = 0;
-        while(start < end) {
+        while(start < end) { // 자를 수 있는 선의 길이의 최대 길이를 찾는다.
             half = (start + end) / 2;
-
+            if(half == 0) break;
 
             int sum = 0;
             for(int i = 0; i < K; i++) sum += wire[i] / half;
-            System.out.println("start : " + start + " end : " + end);
-            System.out.println("half : " + half + " sum : " + sum);
 
-//            if(sum == N) break;
-            if(start == end && sum < N) {
-                half -= 1;
-                break;
-            }
+            if(sum < N) end = half; // 원하는 랜선 수보다 잘라진 랜선 수가 적을 경우, 하나의 랜선 길이가 길어서 길이를 더 짧게 만들어야한다. half보다 아래 수
+            else start = half + 1; // 원하는 랜선 수보다 잘라진 랜선 수가 많을 경우, 하나의 랜선 길이가 짧아서 더 길게 만들어야한다. half보다 위의 수
 
-            if(sum < N) end = half;
-            else start = half;
+//            System.out.println("start : " + start + " end : " + end + " half : " + half);
 
-            if(start >= half) {
-                start += 1;
-                half -= 1;
-            }
-//            else start += 1;
         }
-        System.out.println(half);
+        if(flag && K == N) System.out.println(wire[0]);
+        else System.out.println(start - 1);
         br.close();
     }
 }

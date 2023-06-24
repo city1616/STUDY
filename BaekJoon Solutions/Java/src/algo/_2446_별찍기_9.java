578b92ae185a81c7192f7a21c3355e6204c17aca
city1616 <city1616@naver.com>
2023-06-24 12:44:03 +0900

    Add Solution

---
+++ BaekJoon Solutions/Java/src/algo/_2446_별찍기_9.java
@@ -0,0 +1,27 @@
+package algo;
+
+import java.io.*;
+
+public class _2446_별찍기_9 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < i; j++) sb.append(" ");
+            for(int j = 0; j < (N - i) * 2 - 1; j++) sb.append("*");
+            for(int j = 0; j < i; j++) sb.append(" ");
+            sb.append("\n");
+        }
+        for(int i = N - 1; i > 0; i--) {
+            for(int j = 0; j < i - 1; j++) sb.append(" ");
+            for(int j = 0; j < (N - i) * 2 + 1; j++) sb.append("*");
+            for(int j = 0; j < i - 1; j++) sb.append(" ");
+            sb.append("\n");
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}

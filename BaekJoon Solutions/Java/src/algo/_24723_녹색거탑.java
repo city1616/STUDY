6ab4f2dd020847bafbb943d3d9029a40e368d32c
city1616@naver.com <city1616@naver.com>
2023-07-03 23:42:04 +0900

    Add Solutions

---
+++ BaekJoon Solutions/Java/src/algo/_24723_녹색거탑.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class _24723_녹색거탑 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println((int) Math.pow(2, N));
+        br.close();
+    }
+}

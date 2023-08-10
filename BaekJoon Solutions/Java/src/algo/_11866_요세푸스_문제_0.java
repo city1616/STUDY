f238060d47e75a76367b554cdeadef78d0862df9
city1616@naver.com <city1616@naver.com>
2023-08-10 23:45:57 +0900

    Add Solutions

---
+++ BaekJoon Solutions/Java/src/algo/_11866_요세푸스_문제_0.java
@@ -0,0 +1,19 @@
+package algo;
+
+import java.io.*;
+import java.util.*;
+
+public class _11866_요세푸스_문제_0 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        for(int i = 1; i <= N; i++) {
+            
+        }
+    }
+}

e05dfb1966f8b7b2b775bbc44d2ffec60b15fd7d
city1616@naver.com <city1616@naver.com>
2023-08-26 22:44:57 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_28278_스택_2.java
+++ BaekJoon Solutions/Java/src/algo/_28278_스택_2.java
@@ -12,7 +12,7 @@ public class _28278_스택_2 {
         ArrayDeque<Integer> q = new ArrayDeque<>();
         for(int i = 0; i < N; i++) {
             String S = br.readLine();
-            
+
             int num = 0;
             if(S.length() > 1) {
                 StringTokenizer st = new StringTokenizer(S, " ");
@@ -23,6 +23,8 @@ public class _28278_스택_2 {
             } else {
                 num = Integer.parseInt(S);
             }
+
+            
 
             if(num == 2) {
                 if(!q.isEmpty()) {

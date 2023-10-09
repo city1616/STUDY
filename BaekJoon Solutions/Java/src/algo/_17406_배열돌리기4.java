77c2a06d9cfc0197cd735953a29e48cbcc9e30d3
city1616@naver.com <city1616@naver.com>
2023-10-09 22:49:47 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_17406_배열돌리기4.java
+++ BaekJoon Solutions/Java/src/algo/_17406_배열돌리기4.java
@@ -16,6 +16,7 @@ public class _17406_배열돌리기4 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());

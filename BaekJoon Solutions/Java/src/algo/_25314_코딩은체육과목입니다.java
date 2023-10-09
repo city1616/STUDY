b83637b50064a701201bc3db17ad0793c96bdc4d
city1616@naver.com <city1616@naver.com>
2023-10-10 01:00:51 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_25314_코딩은체육과목입니다.java
+++ BaekJoon Solutions/Java/src/algo/_25314_코딩은체육과목입니다.java
@@ -4,7 +4,6 @@ public class _25314_코딩은체육과목입니다 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
         int N = Integer.parseInt(br.readLine());
         int cnt = N / 4;
         for(int i = 0; i < cnt; i++) {

61be711c4dccf0bac3f166418bd5b014f85a1225
city1616@naver.com <city1616@naver.com>
2023-10-29 23:10:15 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_1463_1로만들기.java
+++ BaekJoon Solutions/Java/src/algo/_1463_1로만들기.java
@@ -7,10 +7,10 @@ public class _1463_1로만들기 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		
 		int[] dp = new int[N + 1];
 		dp[0] = 0;
 		dp[1] = 0;
+
 		for(int i = 2; i <= N; i++) {
 			dp[i] = dp[i - 1] + 1;
 			if(i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);

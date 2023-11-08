10f80be61916f1725cbd70ede8187f52d1c42eeb
city1616@naver.com <city1616@naver.com>
2023-11-08 16:00:12 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_2420_사파리월드.java
+++ BaekJoon Solutions/Java/src/algo/_2420_사파리월드.java
@@ -7,6 +7,7 @@ public class _2420_사파리월드 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
 		long N = Long.parseLong(st.nextToken());
 		long M = Long.parseLong(st.nextToken());
 		

0e0a60cfc45c5fb7aee32c6b6d1295dd57da1c73
city1616@naver.com <city1616@naver.com>
2023-11-09 23:18:22 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_2443_별찍기_6.java
+++ BaekJoon Solutions/Java/src/algo/_2443_별찍기_6.java
@@ -6,6 +6,7 @@ public class _2443_별찍기_6 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
+		
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < i; j++) {
 				System.out.print(" ");

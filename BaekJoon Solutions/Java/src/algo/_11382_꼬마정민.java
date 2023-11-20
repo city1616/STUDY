6ef642c234b7ab52ffd8cd5a84aeddb7ed1797b4
city1616 <city1616@naver.com>
2023-11-20 23:54:14 +0900

    Add Solution

--- BaekJoon Solutions/Java/src/algo/_11382_꼬마정민.java
+++ BaekJoon Solutions/Java/src/algo/_11382_꼬마정민.java
@@ -8,6 +8,7 @@ public class _11382_꼬마정민 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
         long res = 0;
         for(int i = 0; i < 3; i++) res += Long.parseLong(st.nextToken());
         System.out.println(res);

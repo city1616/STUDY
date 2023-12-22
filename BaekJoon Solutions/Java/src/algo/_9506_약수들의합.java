c4282b248f971b218a41211ac09a3195861207cd
city1616@naver.com <city1616@naver.com>
2023-12-22 22:28:48 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_9506_약수들의합.java
+++ BaekJoon Solutions/Java/src/algo/_9506_약수들의합.java
@@ -7,7 +7,6 @@ public class _9506_약수들의합 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
         while(true) {
             int N = Integer.parseInt(br.readLine());
             if(N == -1) {
@@ -21,7 +20,6 @@ public class _9506_약수들의합 {
                     sum += i;
                 }
             }
-            
             if(sum == N) {
                 sb.append(N).append(" = 1");
                 for(int i = 1; i < N; i++) {

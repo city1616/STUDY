d9428d411772751c3c681df331757bda7613b042
city1616@naver.com <city1616@naver.com>
2023-12-24 10:53:48 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_1120_문자열.java
+++ BaekJoon Solutions/Java/src/algo/_1120_문자열.java
@@ -7,6 +7,7 @@ public class _1120_문자열 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
 		String A = st.nextToken();
 		String B = st.nextToken();
 		int cnt = 0;
@@ -21,6 +22,7 @@ public class _1120_문자열 {
 			}
 			if(cnt < min) min = cnt;
 		}
+
 		System.out.println(min);
 		br.close();
 	}

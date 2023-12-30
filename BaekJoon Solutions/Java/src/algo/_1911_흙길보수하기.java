13af530e2f452dadf5e96619059efb85c10e56ea
city1616@naver.com <city1616@naver.com>
2023-12-30 15:27:58 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_1911_흙길보수하기.java
+++ BaekJoon Solutions/Java/src/algo/_1911_흙길보수하기.java
@@ -7,17 +7,14 @@ public class _1911_흙길보수하기 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
         int N = Integer.parseInt(st.nextToken());
         int L = Integer.parseInt(st.nextToken());
-
         int[][] arr = new int[N][2];
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(arr, (o1, o2) -> {
             if(o1[0] == o2[0]) {
                 return o1[1] - o2[1];

38fcc9b1a32aa0ebbc83a1deca828be6934084e6
city1616 <city1616@naver.com>
2023-12-31 12:10:56 +0900

    Add Solution

--- BaekJoon Solutions/Java/src/algo/_2525_오븐_시계.java
+++ BaekJoon Solutions/Java/src/algo/_2525_오븐_시계.java
@@ -5,19 +5,15 @@ import java.util.Scanner;
 public class _2525_오븐_시계 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		int H = sc.nextInt();
 		int M = sc.nextInt();
 		int C = sc.nextInt();
-		
 		H += C / 60;
 		M += C % 60;
-		
 		if(M >= 60) {
 			H += M / 60;
 			M = M % 60;
 		}
-		
 		if(H >= 24) {
 			H -= 24;
 		}

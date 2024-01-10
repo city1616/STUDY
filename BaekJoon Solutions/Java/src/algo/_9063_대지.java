16bcb77d341079b55ba5edd00449ec2ab356f5d4
city1616 <city1616@naver.com>
2024-01-10 22:11:47 +0900

    Add Solution

--- BaekJoon Solutions/Java/src/algo/_9063_대지.java
+++ BaekJoon Solutions/Java/src/algo/_9063_대지.java
@@ -7,13 +7,16 @@ public class _9063_대지 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+
         int[][] arr = new int[2][2];
+
         arr[0][0] = 10000;
         arr[0][1] = -10000;
         arr[1][0] = 10000;
         arr[1][1] = -10000;
 
         int N = Integer.parseInt(br.readLine());
+
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int x = Integer.parseInt(st.nextToken());

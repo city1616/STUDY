e190762ce1c74d7a2568df469f24d45b9ac6c0f5
city1616 <city1616@naver.com>
2024-01-19 10:00:31 +0900

    Add Solution

--- BaekJoon Solutions/Java/src/algo/_1021_회전하는큐.java
+++ BaekJoon Solutions/Java/src/algo/_1021_회전하는큐.java
@@ -8,18 +8,14 @@ public class _1021_회전하는큐 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int ans = 0;
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
         ArrayDeque<Integer> q = new ArrayDeque<>();
         for(int i = 0; i < N; i++) q.offer(i + 1);
-
         st = new StringTokenizer(br.readLine(), " ");
         for(int i = 0; i < M; i++) {
             int num = Integer.parseInt(st.nextToken());
             int cnt = 0;
-
             while(true) {
                 int now = q.poll();
                 if(now == num) break;

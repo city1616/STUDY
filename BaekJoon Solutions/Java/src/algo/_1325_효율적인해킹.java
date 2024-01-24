648efb5f86981d6e0203da6b83bcab39a20e1e38
city1616@naver.com <city1616@naver.com>
2024-01-24 17:07:14 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_1325_효율적인해킹.java
+++ BaekJoon Solutions/Java/src/algo/_1325_효율적인해킹.java
@@ -12,7 +12,6 @@ public class _1325_효율적인해킹 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         String[] nm = br.readLine().split(" ");
         int n = Integer.parseInt(nm[0]);
         int m = Integer.parseInt(nm[1]);

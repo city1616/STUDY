28f96c9cf7d9f3ee1fa5f172d5228255c993ce78
city1616@naver.com <city1616@naver.com>
2024-02-06 23:19:30 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_1330_두_수_비교하기.java
+++ BaekJoon Solutions/Java/src/algo/_1330_두_수_비교하기.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class _1330_두_수_비교하기 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
 		int A = sc.nextInt();
 		int B = sc.nextInt();
 		String result = null;

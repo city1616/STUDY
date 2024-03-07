f80904d4f1e74a9d52fb66e14b2dae79e110820d
city1616@naver.com <city1616@naver.com>
2024-03-07 17:39:27 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_10101_삼각형외우기.java
+++ BaekJoon Solutions/Java/src/algo/_10101_삼각형외우기.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class _10101_삼각형외우기 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int A = Integer.parseInt(br.readLine());
         int B = Integer.parseInt(br.readLine());
         int C = Integer.parseInt(br.readLine());

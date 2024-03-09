c460e4975b320040c8232c26106a10f88e3b4670
city1616@naver.com <city1616@naver.com>
2024-03-09 23:01:09 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_1037_약수.java
+++ BaekJoon Solutions/Java/src/algo/_1037_약수.java
@@ -6,9 +6,11 @@ import java.util.*;
 public class _1037_약수 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
 		for(int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}

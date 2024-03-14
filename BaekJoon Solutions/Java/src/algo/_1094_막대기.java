fffac8069b5ab7305661d316b9a094e67e2e3b01
city1616@naver.com <city1616@naver.com>
2024-03-15 01:16:54 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_1094_막대기.java
+++ BaekJoon Solutions/Java/src/algo/_1094_막대기.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class _1094_막대기 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int X = Integer.parseInt(br.readLine());
 		int[] arr = {64, 32, 16, 8, 4, 2, 1};
 		int cnt = 0;

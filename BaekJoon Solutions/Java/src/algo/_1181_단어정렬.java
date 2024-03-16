b1069dcaaa4d801492484c12788c185e37273e74
city1616@naver.com <city1616@naver.com>
2024-03-16 23:39:36 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_1181_단어정렬.java
+++ BaekJoon Solutions/Java/src/algo/_1181_단어정렬.java
@@ -12,6 +12,7 @@ public class _1181_단어정렬 {
 		for(int i = 0; i < N; i++) {
 			set.add(br.readLine());
 		}
+
 		String[] arr = set.toArray(new String[0]);
 		Arrays.sort(arr, (o1, o2) -> {
 			if(o1.length() == o2.length()) return o1.compareTo(o2);

bbfd42ccc1b5862e22c3fcc444f42cd7278e2f4b
city1616@naver.com <city1616@naver.com>
2024-03-20 18:13:50 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_7568_덩치_2.java
+++ BaekJoon Solutions/Java/src/algo/_7568_덩치_2.java
@@ -9,6 +9,7 @@ public class _7568_덩치_2 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+
         int N = Integer.parseInt(br.readLine());
         int[][] arr = new int[N][3];
 

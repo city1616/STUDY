8cca28aff629661f611476c65f12153e0e48bdc4
city1616@naver.com <city1616@naver.com>
2024-03-25 23:33:45 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_1181_단어_정렬.java
+++ BaekJoon Solutions/Java/src/algo/_1181_단어_정렬.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class _1181_단어_정렬 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		int N = sc.nextInt();
 		String[] word = new String[N];
 		for(int i = 0; i < N; i++) {

102d1f1edf76ff74872cc41a5a1da784a41b8e77
city1616@naver.com <city1616@naver.com>
2024-03-28 23:38:36 +0900

    Add Solutions

--- BaekJoon Solutions/Java/src/algo/_1316_그룹단어체커.java
+++ BaekJoon Solutions/Java/src/algo/_1316_그룹단어체커.java
@@ -25,6 +25,7 @@ public class _1316_그룹단어체커 {
 			// System.out.println(list);
 			Collections.sort(list);
 			// System.out.println(list);
+
 			for(int i = 0; i < list.size() - 1; i++) {
 				if(list.get(i) == list.get(i + 1)) chk = true;
 			}
